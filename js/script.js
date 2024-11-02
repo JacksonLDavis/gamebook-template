5c9e2ad4e94fec979b3461f6f0ddb6c66ed8f536
Jackson Davis <jackson.davis@usask.ca>
2024-11-01 19:46:06 -0600

    Added basic functionality and style
    Basic functionality includes being able to switch pages using buttons

--- js/script.js
+++ js/script.js
@@ -1,2 +1,23 @@
 // Code written by Jackson L. Davis
 
+// when index.html is opened, show the first page
+let initialPage = "intro"
+showPage(initialPage)
+
+/**
+ * Shows the specified page
+ * 
+ * @param {string} pageName: the ID of the page to show, page IDs must be unique
+ */
+function showPage(pageName) {
+    let pages = document.getElementsByClassName("page");
+
+    // hide all pages
+    for (i = 0; i < pages.length; i++) {
+        pages[i].style.display = "none";
+    }
+
+    // show the current page
+    let pageToShow = document.getElementById(pageName);
+    pageToShow.style.display = "block";
+}
